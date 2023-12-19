19a783aaceb2f8aad6e52b5a254e67e1936d2593
Philipp Kotte <philippkotte@icloud.com>
2023-12-19 16:31:00 +0100

    Initial Commit

---
+++ away/awayApp.swift
@@ -0,0 +1,17 @@
+//
+//  awayApp.swift
+//  away
+//
+//  Created by Philipp Kotte on 19.12.23.
+//
+
+import SwiftUI
+
+@main
+struct awayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
